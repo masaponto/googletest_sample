6cd72ec55364593d13c169f65f0fa1886ef20365
masaponto <masaponto@gmail.com>
2017-11-17 23:17:54 +0900

    [Add] func

--- my_app.cpp
+++ my_app.cpp
@@ -3,3 +3,8 @@
 int MyApp::multiply(int i, int j) {
     return i * j;
 }
+
+
+int add(int i, int j) {
+    return i + j;
+}
--- test/my_app_test.cpp
+++ test/my_app_test.cpp
@@ -1,7 +1,12 @@
-#include "../my_app.hpp"
+#include "../my_app.cpp"
 #include "gtest/gtest.h"
 
 TEST(multiply, base)
 {
     EXPECT_EQ(1, MyApp::multiply(1, 1));
+}
+
+TEST(add, base)
+{
+    EXPECT_EQ(2, add(1, 1));
 }
